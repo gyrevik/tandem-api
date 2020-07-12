79537e7034b6fb422e185b7e3463ee85724c9807
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-12 01:17:12 -0400

    latest generated server.js

--- server/server.js
+++ server/server.js
@@ -11,16 +11,13 @@ app.use(function (req, res, next) {
     next();
 });
 app.get('/', function (req, res) {
-    res.send('Hello 8080!');
-});
-app.get('/test', function (req, res) {
-    res.send('test view');
+    res.send('Hello World!');
 });
 app.get('/1234', function (req, res) {
     res.json(data_1234);
 });
 app.get('/4321', function (req, res) {
-    res.send(data_4321);
+    res.json(data_4321);
 });
 app.listen(8080, function () {
     console.log('App is listening on port 8080!');
