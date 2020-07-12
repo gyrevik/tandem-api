5edde68ec27c3c406110dbe488bb64624b5391c4
Alex Smotritsky <alexsmotritsky@MacBook-Pro.home>
2020-07-11 23:04:48 -0400

    added test end point

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
     res.send('Hello 8080!');
 });
 
+app.get('/test', function (req, res) {
+    res.send('test view')
+});
+
 app.listen(8080, function () {
     console.log('App is listening on port 8080!');
 });
